da7a3e8fdd849ac65b4ecd8af898d995283ebe93
kingslay <kingslay@icloud.com>
2013-11-14 16:30:55 +0800

    Initial Commit

---
+++ ScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollView
+//
+//  Created by kingslay on 11/14/13.
+//  Copyright (c) 2013 王 金辨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScrollView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollView
+//
+//  Created by kingslay on 11/14/13.
+//  Copyright (c) 2013 王 金辨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
