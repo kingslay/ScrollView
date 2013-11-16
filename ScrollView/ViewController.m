41d78468e34a0aa3698fc4606eee8fb11cb64c13
kingslay <kingslay@icloud.com>
2013-11-16 13:54:34 +0800

    update

--- ScrollView/ViewController.m
+++ ScrollView/ViewController.m
@@ -17,8 +17,53 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    UITableView *tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 320, 1000)];
+    tableview.delegate = self;
+    tableview.backgroundColor = [UIColor orangeColor];
+    UITextField  *titleField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, tableview.rowHeight)];
+    titleField.delegate = self;
+    titleField.placeholder = NSLocalizedString(@"Add a new item", @"Placeholder text for adding a new item");
+    tableview.tableHeaderView = titleField;
+    
+    
+    [self.view addSubview:tableview];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, -460, 320, 460)];
+    [tableview addSubview:view];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 430, 320, 30)];
+    label.text = @"Hello World!";
+    [view addSubview:label];
+    UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(0, 530, 320, 30)];
+    label2.text = @"Hello World! 2";
+    [view addSubview:label2];
+    NSLog(@"tableview.contentSize= {%f,%f}",tableview.contentSize.width,tableview.contentSize.height);
+    
 }
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    NSLog(@"scrollViewDidScroll");
+    NSLog(@"contentOffset.y=%f",scrollView.contentOffset.y);
+    if (scrollView.contentOffset.y<=-35) {
+        scrollView.contentInset = (UIEdgeInsets){
+            .top=30,
+            .left=0,
+            .bottom=0,
+            .right=0
+        };
+    }
+}
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    NSLog(@"scrollViewDidEndDecelerating");
+    NSLog(@"scrollView.contentSize={%f,%f}",scrollView.contentSize.width,scrollView.contentSize.height);
+    
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
